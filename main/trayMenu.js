9d2ee85b7ef833e765aef151794df975a3cbe573
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2017-05-25 20:47:07 -0500

    Change Tray Menu order

--- main/trayMenu.js
+++ main/trayMenu.js
@@ -1,17 +1,18 @@
 const electron = require("electron");
-const { Menu } = electron;
+const {
+  Menu
+} = electron;
 const hotkeyWindow = require("./hotkeyWindow");
 
-const menu = Menu.buildFromTemplate([
-  {
-    role: "quit"
-  },
-  {
+const menu = Menu.buildFromTemplate([{
     label: "Change HotKey",
-    click: function() {
+    click: function () {
       const win = hotkeyWindow.createHotkeyWindow();
       win.show();
     }
+  },
+  {
+    role: "quit"
   }
 ]);
 
