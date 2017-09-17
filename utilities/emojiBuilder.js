e6cbc9612131fb73e731545312e7b73111334dd7
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2017-09-16 23:51:36 -0500

    Fetch categorized emojis

--- utilities/emojiBuilder.js
+++ utilities/emojiBuilder.js
@@ -1,62 +1,90 @@
-const fs = require("fs");
-const readline = require("readline");
-const path = require("path");
+"use strict";
 
-const inputFilePath = path.resolve(__dirname, "..", "data", "emoji-test.txt");
-const outputFilePath = path.resolve(__dirname, "..", "data", "emojis.json");
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-let data = [];
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var fs = require("fs");
+var readline = require("readline");
+var path = require("path");
+// const _ = require("lodash");
+
+var inputFilePath = path.resolve(__dirname, "..", "data", "emoji-test.txt");
+var outputFilePath = path.resolve(__dirname, "..", "data", "emojis.json");
+
+var data = [];
 
 // Create output file if doesn't exist
 
 fs.closeSync(fs.openSync(outputFilePath, "w"));
-const inputStream = fs.createReadStream(inputFilePath, { encoding: "utf-8" });
-const outputStream = fs.createWriteStream(outputFilePath, {
+var inputStream = fs.createReadStream(inputFilePath, {
+  encoding: "utf-8"
+});
+var outputStream = fs.createWriteStream(outputFilePath, {
   encoding: "utf-8"
 });
 
-outputStream.on("error", error => {
+outputStream.on("error", function (error) {
   console.error("It's error", error);
 });
 
-const rl = readline.createInterface({
+var rl = readline.createInterface({
   input: inputStream,
   terminal: false
 });
 
-i = 100;
-let currentGroup, currentSubgroup, currentEmoji;
+var currentGroup = void 0,
+    currentSubgroup = void 0,
+    currentEmoji = void 0;
+
+var print = function print(data) {
+  console.log(JSON.stringify(data, null, 2));
+};
 
 function createEmoji(emojiArray) {
-  const unicode = emojiArray[1];
-  const emoji = emojiArray[2];
-  const name = emojiArray[3];
+  var unicode = emojiArray[1].trim();
+  var emoji = emojiArray[2];
+  var name = emojiArray[3].trim();
   return {
-    unicode,
-    emoji,
-    name
+    unicode: unicode,
+    emoji: emoji,
+    name: name
   };
 }
 
-const checkGroupRegExp = new RegExp("#\\s*group:\\s*(.*)", "i");
-const checkSubgroupRegExp = new RegExp("#\\s*subgroup:\\s*(.*)", "i");
-const checkEmojiRegExp = new RegExp("(\\w+)\\s*;.*#\\s*([^\\s]+)\\s*(.*)", "i");
+var checkGroupRegExp = new RegExp("#\\s*group:\\s*(.*)", "i");
+var checkSubgroupRegExp = new RegExp("#\\s*subgroup:\\s*(.*)", "i");
+var checkEmojiRegExp = new RegExp("(.*);.*#\\s*([^\\s]+)\\s*(.*)", "i");
 
-rl.on("line", line => {
+var group = void 0,
+    subgroup = void 0,
+    emoji = void 0;
+var id = 1;
+
+rl.on("line", function (line) {
   if ((group = line.match(checkGroupRegExp)) !== null) {
-    const groupName = group[1];
+    var groupName = group[1];
     currentGroup = groupName;
-    console.log(groupName);
-
-    data = Object.assign({}, data, {
-      [groupName]: []
-    });
+    if (!data.hasOwnProperty(currentGroup)) {
+      data = _extends({}, data, _defineProperty({}, currentGroup, {}));
+    }
+    // print(data);
   } else if ((subgroup = line.match(checkSubgroupRegExp)) !== null) {
+    var subgroupName = subgroup[1];
+    currentSubgroup = subgroupName;
+    // print(data);
+    if (!data[currentGroup].hasOwnProperty(currentSubgroup)) {
+      data = _extends({}, data, _defineProperty({}, currentGroup, _extends({}, data[currentGroup], _defineProperty({}, currentSubgroup, {}))));
+    }
   } else if ((emoji = line.match(checkEmojiRegExp)) !== null) {
+    var emojiObj = createEmoji(emoji);
+    if (currentGroup && currentSubgroup) {
+      data = _extends({}, data, _defineProperty({}, currentGroup, _extends({}, data[currentGroup], _defineProperty({}, currentSubgroup, _extends({}, data[currentGroup][currentSubgroup], _defineProperty({}, emojiObj.unicode, emojiObj))))));
+    }
   }
 });
 
-rl.on("close", () => {
+rl.on("close", function () {
   outputStream.write(JSON.stringify(data, null, 2));
   console.log("Finished");
 });
---
+++ utilities/emojiBuilderWithSpread.js
@@ -0,0 +1,98 @@
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
+// const _ = require("lodash");
+
+const inputFilePath = path.resolve(__dirname, "..", "data", "emoji-test.txt");
+const outputFilePath = path.resolve(__dirname, "..", "data", "emojis.json");
+
+let data = [];
+
+// Create output file if doesn't exist
+
+fs.closeSync(fs.openSync(outputFilePath, "w"));
+const inputStream = fs.createReadStream(inputFilePath, {
+  encoding: "utf-8"
+});
+const outputStream = fs.createWriteStream(outputFilePath, {
+  encoding: "utf-8"
+});
+
+outputStream.on("error", error => {
+  console.error("It's error", error);
+});
+
+const rl = readline.createInterface({
+  input: inputStream,
+  terminal: false
+});
+
+let currentGroup, currentSubgroup, currentEmoji;
+
+const print = data => {
+  console.log(JSON.stringify(data, null, 2));
+};
+
+function createEmoji(emojiArray) {
+  const unicode = emojiArray[1].trim();
+  const emoji = emojiArray[2];
+  const name = emojiArray[3].trim();
+  return {
+    unicode,
+    emoji,
+    name
+  };
+}
+
+const checkGroupRegExp = new RegExp("#\\s*group:\\s*(.*)", "i");
+const checkSubgroupRegExp = new RegExp("#\\s*subgroup:\\s*(.*)", "i");
+const checkEmojiRegExp = new RegExp("(.*);.*#\\s*([^\\s]+)\\s*(.*)", "i");
+
+let group, subgroup, emoji;
+let id = 1;
+
+rl.on("line", line => {
+  if ((group = line.match(checkGroupRegExp)) !== null) {
+    const groupName = group[1];
+    currentGroup = groupName;
+    if (!data.hasOwnProperty(currentGroup)) {
+      data = {
+        ...data,
+        [currentGroup]: {}
+      };
+    }
+    // print(data);
+  } else if ((subgroup = line.match(checkSubgroupRegExp)) !== null) {
+    const subgroupName = subgroup[1];
+    currentSubgroup = subgroupName;
+    // print(data);
+    if (!data[currentGroup].hasOwnProperty(currentSubgroup)) {
+      data = {
+        ...data,
+        [currentGroup]: {
+          ...data[currentGroup],
+          [currentSubgroup]: {}
+        }
+      };
+    }
+  } else if ((emoji = line.match(checkEmojiRegExp)) !== null) {
+    const emojiObj = createEmoji(emoji);
+    if (currentGroup && currentSubgroup) {
+      data = {
+        ...data,
+        [currentGroup]: {
+          ...data[currentGroup],
+          [currentSubgroup]: {
+            ...data[currentGroup][currentSubgroup],
+            [emojiObj.unicode]: emojiObj
+          }
+        }
+      };
+    }
+  }
+});
+
+rl.on("close", () => {
+  outputStream.write(JSON.stringify(data, null, 2));
+  console.log("Finished");
+});
